3fefbf40c7e4f6a4bfd00dd9de06b9de5b2a4df1
Metre <xianwang.mi@genlot.com>
2019-10-28 17:12:02 +0800

    增加AMap插件

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:amap_base_map/amap_base_map.dart';
 
-void main() => runApp(MyApp());
+void main() async{
+  await AMap.init('27d67839721288be2ddd87b4fd868822');
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -74,31 +78,17 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        child:  AMapView(
+            onAMapViewCreated: (controller) { },
+            amapOptions: AMapOptions(
+              compassEnabled: false,
+              zoomControlsEnabled: true,
+              logoPosition: LOGO_POSITION_BOTTOM_CENTER,
+              camera: CameraPosition(
+                target: LatLng(40.851827, 111.801637),
+                zoom: 15,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
